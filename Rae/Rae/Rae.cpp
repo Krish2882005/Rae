9f5f4c627bbc561e766501f1792224d140e01416
Krish <ganatrakrish2882005@gmail.com>
2025-10-24 16:30:02 +0530

    build: add version generation template

---
+++ Rae/Rae/Rae.cpp
@@ -0,0 +1,7 @@
+#include "Rae/Rae.hpp"
+
+namespace Rae
+{
+    // Empty implementation file to ensure compile_commands.json generation
+    // This enables IntelliSense and other tooling to work correctly
+}
--- Rae/Rae/Rae.hpp
+++ Rae/Rae/Rae.hpp
@@ -1,1 +1,3 @@
 #pragma once
+
+#include "Rae/Config/Version.hpp"
